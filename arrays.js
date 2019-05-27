a5f2a24e08c879e317f58b3d9210453e131f36e9
Klea <laskaklea@gmail.com>
2019-05-28 00:07:08 +0200

    Functions in arrays

--- arrays.js
+++ arrays.js
@@ -19,3 +19,16 @@ for (var element in fruits )
 console.log(fruits[element]);
 //8
 console.log(fruits.length);
+//9
+(fruits.filter(elem=>elem.length>=6)).forEach(elem=>console.log(elem));
+
+//10 
+
+
+console.log("----------");
+function find(fruits){
+    return (fruits.filter(elem=>elem.length>=6)).forEach(elem=>console.log(elem));
+}
+find(fruits);
+
+
